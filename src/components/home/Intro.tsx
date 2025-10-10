dfb4a6b7b2f107f08c97790ef8506dde5dc2590f
TrivexMalith <malith.trivextech@gmail.com>
2025-10-10 18:27:03 +0530

    changes

--- src/components/home/Intro.tsx
+++ src/components/home/Intro.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const Intro = () => {
     return (
-        <div className='flex flex-col px-5 md:px-20 xl:px-40 my-24 gap-y-4'>
+        <div className='flex flex-col px-5 md:px-20 xl:px-56 my-24 gap-y-4'>
             <h3 className='text-lg font-work-sans text-sea-green uppercase text-center font-medium'>Welcome to Travel Nation</h3>
             <h2 className='text-4xl sm:text-6xl text-sea-green font-semibold text-center'>Your Gateway to the Maldives and Sri Lanka!</h2>
             <p className='text-lg text-gray-500 text-center py-6'>At Travel Nation, we&apos;re not just about crafting holidays; we&apos;re about creating unforgettable journeys that immerse you in the heart of two of the world&apos;s most stunning destinations – the Maldives and Sri Lanka. Whether you&apos;re dreaming of pristine beaches, vibrant culture, or untamed wilderness, we&apos;ve got you covered.
